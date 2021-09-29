cf5cf62ec7991710f86d01cf5fee77691c7dcdf7
Shubhankar Thakur <shubhankarthakur75@.com>
2021-09-29 12:25:15 +0530

    updated

--- main.js
+++ main.js
@@ -74,30 +74,83 @@ setInterval(greetMessage, 1000);
 
 let startWakeUpTime;
 let endWakeUpTime;
-let selectedWakeUpTime = document.getElementById('wake-up-time');
-selectedWakeUpTime.addEventListener("change", () => {
-    let wakeUpTime = selectedWakeUpTime.options[selectedWakeUpTime.selectedIndex].value.split("-");
-    startWakeUpTime = wakeUpTime[0];
-    endWakeUpTime = wakeUpTime[1];
+let wakeUpTime = document.getElementById('wake-up-time');
+
+function addErrorMessage(errorMessageId,errorDropbox) {
+    let scheduleFormDropdown = document.querySelector(".schedule-form-dropdown");
+    errorDropbox.style.borderColor = "red";
+    errorMessageId.classList.add("schedule-form-error-message");
+}
+
+function removeErrorMessage(errorMessageId,errorDropbox) {
+    let scheduleFormDropdown = document.querySelector(".schedule-form-dropdown");
+    errorMessageId.classList.toggle("schedule-form-error-message");
+    errorMessageId.innerText = "";
+    errorDropbox.style.borderColor = "#d68686";
+}
+
+wakeUpTime.addEventListener("change", () => {
+    let selectedWakeUpTime = wakeUpTime.options[wakeUpTime.selectedIndex].value.split("-");
+    console.log("addEventListener");
+    console.log(selectedWakeUpTime[0]);
+    console.log(selectedWakeUpTime[1]);
+
+
+    if (((selectedWakeUpTime[0] != startLunchTime) && (selectedWakeUpTime[1] != endLunchTime)) && ((selectedWakeUpTime[0] != startSleepTime) && (selectedWakeUpTime[1] != endSleepTime))) {
+        startWakeUpTime = selectedWakeUpTime[0];
+        endWakeUpTime = selectedWakeUpTime[1];
+        let errorMessage = document.getElementById("wake-up-error-message");
+        let errorDropbox=document.getElementById("wake-up-time");
+        removeErrorMessage(errorMessage,errorDropbox);
+    }
+    else {
+        let errorMessage = document.getElementById("wake-up-error-message");
+        let errorDropbox=document.getElementById("wake-up-time");
+        errorMessage.innerText = "*This slot is already booked.Please reselect.";
+        addErrorMessage(errorMessage,errorDropbox);
+    }
 });
 
 
 let startLunchTime;
 let endLunchTime;
-let selectedLunchTime = document.getElementById('lunch-time');
-selectedLunchTime.addEventListener("change", () => {
-    let lunchTime = selectedLunchTime.options[selectedLunchTime.selectedIndex].value.split("-");
-    startLunchTime = lunchTime[0];
-    endLunchTime = lunchTime[1];
+let lunchTime = document.getElementById('lunch-time');
+lunchTime.addEventListener("change", () => {
+    let selectedLunchTime = lunchTime.options[lunchTime.selectedIndex].value.split("-");
+    if (((selectedLunchTime[0] != startWakeUpTime) && (selectedLunchTime[1] != endWakeUpTime)) && ((selectedLunchTime[0] != startSleepTime) && (selectedLunchTime[1] != endSleepTime))) {
+        startLunchTime = selectedLunchTime[0];
+        endLunchTime = selectedLunchTime[1];
+        let errorMessage = document.getElementById("lunch-error-message");
+        let errorDropbox=document.getElementById("lunch-time");
+        removeErrorMessage(errorMessage,errorDropbox);
+    }
+    else {
+        let errorMessage = document.getElementById("lunch-error-message");
+        let errorDropbox=document.getElementById("lunch-time");
+        errorMessage.innerText = "*This slot is already booked.Please reselect.";
+        addErrorMessage(errorMessage,errorDropbox);
+    }
 });
 
 let startSleepTime;
 let endSleepTime;
-let selectedSleepTime = document.getElementById('sleep-time');
-selectedSleepTime.addEventListener("change", () => {
-    let sleepTime = selectedSleepTime.options[selectedSleepTime.selectedIndex].value.split("-");
-    startSleepTime = sleepTime[0];
-    endLunchTime = sleepTime[1];
+let sleepTime = document.getElementById('sleep-time');
+sleepTime.addEventListener("change", () => {
+    let selectedSleepTime = sleepTime.options[sleepTime.selectedIndex].value.split("-");
+    if (((selectedSleepTime[0] != startWakeUpTime) && (selectedSleepTime[1] != endWakeUpTime)) && ((selectedSleepTime[0] != startLunchTime) && (selectedSleepTime[1] != endLunchTime))) {
+        startSleepTime = selectedSleepTime[0];
+        endSleepTime = selectedSleepTime[1];
+        let errorMessage = document.getElementById("sleep-error-message");
+        let errorDropbox=document.getElementById("sleep-time");
+        removeErrorMessage(errorMessage,errorDropbox)
+    }
+    else {
+        let errorMessage = document.getElementById("sleep-error-message");
+        let errorDropbox=document.getElementById("sleep-time");
+        errorMessage.innerText = "*This slot is already booked.Please reselect.";
+        addErrorMessage(errorMessage,errorDropbox);
+    }
+
 });
 
 
@@ -113,10 +166,10 @@ function imageHandlerFunction() {
         if (startWakeUpTime === undefined && endWakeUpTime === undefined) {
 
         }
-        else if (hours >= startWakeUpTime && hours <= endWakeUpTime) {
+        else if (hours >= startWakeUpTime && hours < endWakeUpTime) {
             state = 1;
             document.getElementById("index-main-image1").src = "./assets/images/wake-up-image.png";
-            document.querySelector(".index-element-desc").innerText="HEY IT'S WAKE-UP TIME...!! "
+            document.querySelector(".index-image-desc").innerText = "HEY IT'S WAKE-UP TIME...!! "
         }
         else {
             document.getElementById("index-main-image1").src = "./assets/images/main-image.png";
@@ -132,11 +185,11 @@ function imageHandlerFunction() {
 
         if (startLunchTime === undefined && endLunchTime === undefined) {
         }
-        else if (hours >= startLunchTime && hours <= endLunchTime) {
+        else if (hours >= startLunchTime && hours < endLunchTime) {
             state = 1;
             document.getElementById("index-main-image1").src = "./assets/images/pizza-party.png";
-            document.querySelector(".index-image-desc").innerHTML=
-            "HEY IT'S LUNCH TIME...!!";
+            document.querySelector(".index-image-desc").innerHTML =
+                "HEY IT'S LUNCH TIME...!!";
         }
         else {
             state = 0;
@@ -148,13 +201,12 @@ function imageHandlerFunction() {
     // Nap-time
     if (state === 0) {
 
-        if (startSleepTime === undefined && endSleepTime === undefined)
-        {
+        if (startSleepTime === undefined && endSleepTime === undefined) {
         }
-        else if (hours >= startSleepTime && hours <= endSleepTime) {
+        else if (hours >= startSleepTime && hours < endSleepTime) {
             state = 1;
             document.getElementById("index-main-image1").src = "./assets/images/sleep-image.png";
-            document.querySelector(".index-image-desc").innerHTML="HEY IT'S NAP TIME...PLEASE HAVE A SLEEP!! ";
+            document.querySelector(".index-image-desc").innerHTML = "HEY IT'S NAP TIME...PLEASE HAVE A SLEEP!! ";
         }
         else {
             state = 0;
@@ -166,17 +218,17 @@ function imageHandlerFunction() {
 }
 setInterval(imageHandlerFunction, 1000);
 
-let party=document.getElementById("current-task");
-party.addEventListener("click",partyFunction1)
-party.addEventListener("dblclick",partyFunction2)
+let party = document.getElementById("current-task");
+party.addEventListener("click", partyFunction1)
+party.addEventListener("dblclick", partyFunction2)
 
-function partyFunction1(){
+function partyFunction1() {
     document.getElementById("index-main-image1").src = "./assets/images/party-image.jpg";
-    document.querySelector(".index-image-desc").innerHTML="Let's Make a Partyyy!!";
-    document.getElementById("current-task").innerText="Double Click Here !!";
+    document.querySelector(".index-image-desc").innerHTML = "Let's Make a Partyyy!!";
+    document.getElementById("current-task").innerText = "Double Click Here !!";
 }
-function partyFunction2(){
+function partyFunction2() {
     document.getElementById("index-main-image1").src = "./assets/images/main-image.png";
-    document.querySelector(".index-image-desc").innerHTML="";
-    document.getElementById("current-task").innerText="Party Time";
+    document.querySelector(".index-image-desc").innerHTML = "";
+    document.getElementById("current-task").innerText = "Party Time";
 }
